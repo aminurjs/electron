27413a3293ef51826c385749d91cd7a00744999a
Aminur Rahman <dev.aminur@gmail.com>
2025-04-04 12:35:55 +0600

    remove unused files and configurations

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/renderer/**/*.{js,ts,jsx,tsx,html}', './src/**/*.{js,ts,jsx,tsx,html}'],
-  theme: {
-    extend: {}
-  },
-  plugins: []
-}
