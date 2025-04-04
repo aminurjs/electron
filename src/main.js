da7ee524335be120865f38f282ae9592e67d1723
Aminur Rahman <dev.aminur@gmail.com>
2025-04-04 14:32:33 +0600

    add initial Electron app structure with preload and renderer scripts

---
+++ src/main.js
@@ -0,0 +1,33 @@
+require("update-electron-app")();
+const { app, BrowserWindow, ipcMain } = require("electron/main");
+const path = require("node:path");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadFile(path.join(__dirname, "index.html"));
+};
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.whenReady().then(() => {
+  ipcMain.handle("ping", () => "pong");
+
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
